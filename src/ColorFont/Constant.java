63eb2e72e42a7f8b6f51dff5829194fafb8880de
Jason Hoang <jasonhoang6201@gmail.com>
2022-01-07 23:59:11 +0700

    adjust UI mainframe, addframe, fix search and refresh button

--- src/ColorFont/Constant.java
+++ src/ColorFont/Constant.java
@@ -11,4 +11,6 @@ public class Constant {
     static public Font INFO_FONT = new Font(Constant.SYSTEM_FONT, Font.PLAIN, Constant.SYSTEM_FONT_SIZE);
     static public Font LABEL_FONT = new Font(Constant.SYSTEM_FONT, Font.BOLD, Constant.SYSTEM_FONT_SIZE);
     static public Font HEADER_FONT = new Font(Constant.SYSTEM_FONT, Font.BOLD, Constant.SYSTEM_HEADER_SIZE);
+    static public Font TABLE_FONT = new Font(Constant.SYSTEM_FONT, Font.PLAIN, 15);
+    static public Font TABLE_HEADER = new Font(Constant.SYSTEM_FONT, Font.BOLD, 18);
 }
--- src/PaymentFrame/PaymentFrame.java
+++ src/PaymentFrame/PaymentFrame.java
@@ -10,6 +10,8 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -36,13 +38,13 @@ public class PaymentFrame extends JFrame {
     public PaymentFrame()
     {
         setTitle("Manager - Covid Management System");
-//        ImageIcon covid_icon=null;
-//        try {
-//            covid_icon=new ImageIcon(ImageIO.read(new File("rsc/covid_icon.png")));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        setIconImage(covid_icon.getImage());
+        ImageIcon covid_icon=null;
+        try {
+            covid_icon=new ImageIcon(ImageIO.read(new File("rsc/covid_icon.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        setIconImage(covid_icon.getImage());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1024, 720);
         setResizable(false);
@@ -55,40 +57,60 @@ public class PaymentFrame extends JFrame {
 
         JPanel LabelPane = new JPanel(new FlowLayout(FlowLayout.CENTER));
         JLabel BalanceLabel = new JLabel("Balance");
-        BalanceLabel.setFont(Constant.LABEL_FONT);
+        BalanceLabel.setFont(Constant.HEADER_FONT);
         BalanceLabel.setForeground(Constant.my_white);
         LabelPane.setBackground(Constant.my_gray);
         LabelPane.add(BalanceLabel);
         contentPane.add(LabelPane);
 
-        JPanel ButtonChangePane = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        ButtonChangePane.setBackground(Constant.my_gray);
-        JButton AddNewUserButton = new JButton("Add new user");
-        AddNewUserButton.setForeground(Constant.my_white);
-        AddNewUserButton.setBackground(new Color(77,82,77));
+        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        topPanel.setBackground(Constant.my_gray);
+
+        JTextField SearchUserNameField = new JTextField();
+        SearchUserNameField.setFont(Constant.INFO_FONT);
+        SearchUserNameField.setColumns(20);
+        SearchUserNameField.setForeground(Color.GRAY);
+        SearchUserNameField.setText("Username");
+
+        SearchUserNameField.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (SearchUserNameField.getText().equals("Username")) {
+                    SearchUserNameField.setText("");
+                    SearchUserNameField.setForeground(Color.BLACK);
+                }
+            }
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (SearchUserNameField.getText().isEmpty()) {
+                    SearchUserNameField.setForeground(Color.GRAY);
+                    SearchUserNameField.setText("Username");
+                }
+            }
+        });
+
+        JButton SearchUserNameButton = new JButton("Search");
+        SearchUserNameButton.setForeground(Constant.my_white);
+        SearchUserNameButton.setBackground(new Color(77,82,77));
+        SearchUserNameButton.setFont(Constant.INFO_FONT);
 
         JButton RefreshButton = new JButton("Refresh");
         RefreshButton.setForeground(Constant.my_white);
         RefreshButton.setBackground(new Color(77,82,77));
+        RefreshButton.setFont(Constant.INFO_FONT);
 
-        ButtonChangePane.add(AddNewUserButton);
-        ButtonChangePane.add(RefreshButton);
-        contentPane.add(ButtonChangePane);
+        topPanel.add(SearchUserNameField);
+        topPanel.add(SearchUserNameButton);
+        topPanel.add(RefreshButton);
 
-        AddNewUserButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new AddUserByPayment().setVisible(true);
-                PaymentFrame.this.dispose();
-            }
-        });
 
         JScrollPane ListPaymentHistoryCenter = new JScrollPane();
         ListPaymentHistoryCenter.setPreferredSize(new Dimension(780,600));
-        contentPane.add(ListPaymentHistoryCenter,BorderLayout.CENTER);
 
         JTable PaymentHistoryTable = new JTable();
         PaymentHistoryTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        PaymentHistoryTable.getTableHeader().setFont(Constant.TABLE_HEADER);
+        PaymentHistoryTable.setFont(Constant.TABLE_FONT);
 
         PaymentHistoryTable.setModel(def);
         def.addColumn("Username");
@@ -105,6 +127,50 @@ public class PaymentFrame extends JFrame {
         }
         ListPaymentHistoryCenter.setViewportView(PaymentHistoryTable);
 
+        JPanel botPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        botPanel.setBackground(Constant.my_gray);
+
+        JButton AddNewUserButton = new JButton("Add new user");
+        AddNewUserButton.setForeground(Constant.my_white);
+        AddNewUserButton.setBackground(new Color(77,82,77));
+        AddNewUserButton.setFont(Constant.INFO_FONT);
+
+        botPanel.add(AddNewUserButton);
+
+        add(topPanel);
+        add(ListPaymentHistoryCenter);
+        add(botPanel);
+
+        SearchUserNameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                if(!SearchUserNameField.getText().equals("Username"))
+                {
+                    String usernamesearch = SearchUserNameField.getText();
+
+                    ArrayList<payment_history> ListHistoryUser = getDB.PaymentHistory.FunctionPaymentHistory.GetUpdateHistory(usernamesearch);
+
+                    if(ListHistoryUser.size() > 0) {
+                        def.setRowCount(0);
+                        for(payment_history i : ListHistoryUser)
+                        {
+                            def.addRow(new Object[] {
+                                    i.getUsername(), i.getDate_create(), i.getDebt_pay(), i.getNew_balance()
+                            });
+                        }
+                    }
+                    else JOptionPane.showMessageDialog(PaymentFrame.this, "There is no username match your search",
+                            "Search",JOptionPane.INFORMATION_MESSAGE);
+                }
+                else
+                {
+                    JOptionPane.showMessageDialog(PaymentFrame.this, "Please input username",
+                            "Error",JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
         RefreshButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -116,48 +182,17 @@ public class PaymentFrame extends JFrame {
                             i.getUsername(), i.getDate_create(), i.getDebt_pay(), i.getNew_balance()
                     });
                 }
+                SearchUserNameField.setText("Username");
+                SearchUserNameField.setForeground(Color.GRAY);
             }
         });
 
-        JPanel SearchBottomPane = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        contentPane.add(SearchBottomPane);
-        SearchBottomPane.setBackground(Constant.my_gray);
-
-        JTextField SearchUserNameField = new JTextField();
-        SearchUserNameField.setColumns(15);
-
-        JButton SearchUserNameButton = new JButton("Search by UserName");
-        SearchUserNameButton.setForeground(Constant.my_white);
-        SearchUserNameButton.setBackground(new Color(77,82,77));
-
-        SearchUserNameButton.addActionListener(new ActionListener() {
+        AddNewUserButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                if(!SearchUserNameField.getText().equals(""))
-                {
-                    String usernamesearch = SearchUserNameField.getText();
-
-                    ArrayList<payment_history> ListHistoryUser = getDB.PaymentHistory.FunctionPaymentHistory.GetUpdateHistory(usernamesearch);
-
-                        def.setRowCount(0);
-                        for(payment_history i : ListHistoryUser)
-                        {
-                            def.addRow(new Object[] {
-                                    i.getUsername(), i.getDate_create(), i.getDebt_pay(), i.getNew_balance()
-                            });
-                        }
-                }
-                else
-                {
-                    JOptionPane.showMessageDialog(PaymentFrame.this, "Please input username",
-                            "Error",JOptionPane.ERROR_MESSAGE);
-                }
+            public void actionPerformed(ActionEvent e) {
+                new AddUserByPayment().setVisible(true);
             }
         });
-
-        SearchBottomPane.add(SearchUserNameField);
-        SearchBottomPane.add(SearchUserNameButton);
     }
 
 }
